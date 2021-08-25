a360bf83fb401ce1c6fdf714c86d1c064aa409e0
Renato Fernandes Santos Silva <wathergeen@gmail.com>
2021-08-25 07:30:34 -0300

    modified:   README.md 	new file:   assets/css/custom.css 	new file:   assets/css/loader.css 	new file:   assets/css/login.css 	new file:   assets/images/img-login.svg 	new file:   assets/js/loader.js 	new file:   assets/js/login.js 	new file:   login.php

---
+++ README.md
@@ -0,0 +1,1 @@
+### Sistema para notas de Alunos.
---
+++ assets/js/loader.js
@@ -0,0 +1,5 @@
+$(window).on("load", function () {
+    $(".content-wrapper").hide(),
+        $(".loader-wrapper").fadeOut(730),
+        $(".content-wrapper").show(1000);
+});
